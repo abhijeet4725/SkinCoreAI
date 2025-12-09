1ff0238ca14e0913d85065d36f78fa36f41d818a
abhijeet4725 <abhijeetcs4725@gmail.com>
2025-12-09 16:06:38 +0530

    first commit

---
+++ src/main/java/com/example/skincore/SkinCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.skincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skincore/SkinCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skincore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
